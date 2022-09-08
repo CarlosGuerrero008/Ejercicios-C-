7eb22be3b1e20558e27b12048b1b1dc90fa28d24
CARLOS GUERRERO <CARLOS GUERRERO@LAPTOP-RS0CRK9G>
2022-09-08 18:40:07 -0500

    Agregar archivos de proyecto.

---
+++ trabajo 5 de c/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ejercicio5
+{
+    public class program
+    {
+        public static void Main()
+        {
+            
+            int limite;
+            double cuadrado;
+            int numero = 2;
+            int cotador;
+            cotador = 0;
+            limite = 10;
+            for (cotador = 0; cotador <= limite; cotador++)
+            {
+                cuadrado = Math.Pow(numero, cotador);
+                Console.WriteLine($"El resultado del número 2 al potecia de {cotador} es : {cuadrado}");
+            }
+            
+        }
+    }
+}
+
